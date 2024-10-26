be8f5b4b84b4629ba4f4d2254c031e4cb7097fc4
bahar <88434002+bahar-dc@users.noreply.github.com>
2024-10-27 01:52:14 +0500

    Content updates

--- src/Components/Twocolumn.jsx
+++ src/Components/Twocolumn.jsx
@@ -3,22 +3,14 @@ export const Twocolumntext = () => {
         <>
             <section className="two-col-title-list">
                 <div className="site-container">
-					 <h2>Why  <span style={{ color: '#153bff' }}>Choose Us?</span> </h2>
+					 <h2>Why Choose <span style={{ color: '#153bff' }}>Progresso</span> Learning Institute? </h2>
 
                     <ul>
 					 <h4>Three Key Selling Points:</h4>
-						<li>
-							<p className="text">Certified Language Experts</p>
-						</li>
-						<li>
-							
-							<p className="text">Flexible Learning Schedules (Online & In-person)</p>
-						</li>
-						<li>
-							
-							<p className="text">Proven Success with Immigrants & Students
-							</p>
-						</li>
+						<li><p>● <strong>Qualified Instructors</strong> – Skilled professionals with international teaching experience.</p></li>
+						<li><p>● <strong>Flexible Learning</strong> – Online classes tailored to fit your schedule.</p></li>
+						<li><p>● <strong>Real-World Scenarios</strong> – Hands-on practice to apply your learning confidently.</p></li>
+						<li><p>● <strong>Community Support</strong> – Connect with peers and instructors for constant support.</p></li>
 					</ul>
                 </div>
             </section>
