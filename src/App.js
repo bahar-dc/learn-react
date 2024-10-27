06407f7bf5173ff27e2d977c478bf5d6a53a5e26
bahar <88434002+bahar-dc@users.noreply.github.com>
2024-10-28 00:10:20 +0500

    updates for github pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './Assets/Css/css-main.min.css';
 import './Assets/Css/style-main.css';
 import './Assets/Css/style-main-two.css';
 import './Assets/Css/Style.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./Components/HomePage";
 import { About } from "./Components/About";
 import { Pricing } from "./Components/Pricings";
@@ -14,21 +14,20 @@ import { Blog } from "./Components/Blog";
 function App() {
   return (
     <>
-
-     <Router>
-            <div className="App">
-      <Routes>
-        <Route path="/learn-react" element={<HomePage />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/blocks" element={<Blocks />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-            </div>
-    </Router>
-     
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/learn-react" element={<HomePage />} />
+            <Route path="/about-us" element={<About />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/blocks" element={<Blocks />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
+
 
 export default App;
