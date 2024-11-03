b5ff4d33dcc0f15c2e361d677f90d069bbe89629
bahar <88434002+bahar-dc@users.noreply.github.com>
2024-11-03 18:02:36 +0500

    colors

--- src/Components/Learning.jsx
+++ src/Components/Learning.jsx
@@ -15,7 +15,6 @@ export const Learning = () => {
     return (
         <>
             <section className="learning-styles-carousel">
-                <span className="outer-bg" style={{background:'#ECEFFF'}}></span>
                 <div>
                     <div className="site-container learning-section-head">
                         <h2>Courses Tailored for Your  <br /> <span>Success
