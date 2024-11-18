f2c18de4dd8e186f3ef669b422958c1e78aaa532
bahar <88434002+bahar-dc@users.noreply.github.com>
2024-11-18 22:57:30 +0500

    Update HomePage.jsx

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -49,7 +49,7 @@ export const HomePage = () => {
             <Learning />
             <FullWidthScrollImage />
             <StickyWIthText />
-            <Logolist />
+            {/* <Logolist /> */}
 
             <section className="accordion">
                 <div className="progresso-container">
